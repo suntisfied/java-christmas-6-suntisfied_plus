ce0b046bcbe65a182428f7ce117b25cc674183e7
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-09 21:33:22 +0900

    feat(view): check validity of input date

--- src/main/java/christmas/view/input/Date.java
+++ src/main/java/christmas/view/input/Date.java
@@ -1,4 +1,14 @@
 package christmas.view.input;
 
 public record Date(int date) {
+    public Date(int date) {
+        this.date = date;
+        validate(date);
+    }
+
+    private void validate(int date) {
+        if (date < 1 || date > 31) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
