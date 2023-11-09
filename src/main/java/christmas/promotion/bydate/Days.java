5a01ee0a84f24730741ab4a63b3778d096b8b978
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-09 23:17:18 +0900

    feat(promotion): check if input date is weekday

---
+++ src/main/java/christmas/promotion/bydate/Days.java
@@ -0,0 +1,19 @@
+package christmas.promotion.bydate;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Days {
+    WEEKDAYS(Arrays.asList(3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 31)),
+    WEEKENDS(Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30));
+
+    private final List<Integer> days;
+
+    Days(List<Integer> days) {
+        this.days = days;
+    }
+
+    public List<Integer> getDays() {
+        return days;
+    }
+}
---
+++ src/main/java/christmas/promotion/bydate/Weekday.java
@@ -0,0 +1,23 @@
+package christmas.promotion.bydate;
+
+import christmas.promotion.Discount;
+import christmas.view.input.Date;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Weekday implements DateDiscount {
+    @Override
+    public boolean check(Date date) {
+        return isWeekday.test(date);
+    }
+
+    @Override
+    public Discount calculateDiscount(Date date) {
+        return null;
+    }
+
+    Predicate<Date> isWeekday = date -> {
+        List<Integer> weekdays = Days.WEEKDAYS.getDays();
+        return weekdays.contains(date.date());
+    };
+}
