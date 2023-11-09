a8dc07b362f30f5dfda4992ef717de1765849dfd
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-09 23:29:35 +0900

    feat(promotion): check if input date is weekend

---
+++ src/main/java/christmas/promotion/bydate/Weekend.java
@@ -0,0 +1,23 @@
+package christmas.promotion.bydate;
+
+import christmas.promotion.Discount;
+import christmas.view.input.Date;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Weekend implements DateDiscount {
+    @Override
+    public boolean check(Date date) {
+        return isWeekend.test(date);
+    }
+
+    @Override
+    public Discount calculateDiscount(Date date) {
+        return null;
+    }
+
+    Predicate<Date> isWeekend = date -> {
+        List<Integer> weekends = Days.WEEKENDS.getDays();
+        return weekends.contains(date.date());
+    };
+}
