44b1104841afaf9eab9c3c2be65062cb9c4d2172
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-10 21:04:52 +0900

    feat(menu): calculate total sum of ordered items

--- src/main/java/christmas/menu/OrderedTotal.java
+++ src/main/java/christmas/menu/OrderedTotal.java
@@ -1,6 +1,9 @@
 package christmas.menu;
 
 import christmas.menu.converter.MenuConverter;
+import christmas.menu.menuitem.Menu;
+import java.util.HashMap;
+import java.util.List;
 
 public class OrderedTotal {
     private final OrderedMenuTotal orderedMenuTotal;
@@ -11,5 +14,20 @@ public class OrderedTotal {
 
     public OrderedMenus produceOrderedMenu() {
         return new OrderedMenus(orderedMenuTotal.orderedMenuTotal().keySet().stream().toList());
+    }
+
+    public OrderAmount calculateTotalOrderAmount() {
+        HashMap<Menu, MenuAmount> rawOrderedMenuTotal = orderedMenuTotal.orderedMenuTotal();
+        List<Menu> rawOrderedMenus = produceOrderedMenu().orderedMenus();
+
+        int totalSum = 0;
+        for (Menu currentOrderedMenu : rawOrderedMenus) {
+            int currentMenuPrice = currentOrderedMenu.getPrice().price();
+            int currentMenuAmount = rawOrderedMenuTotal.get(currentOrderedMenu).amount();
+
+            totalSum += currentMenuPrice * currentMenuAmount;
+        }
+
+        return new OrderAmount(totalSum);
     }
 }
