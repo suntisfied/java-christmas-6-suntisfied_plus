14a49b16e3801f7218eafaa8d7a3b79cb1c27fc8
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-10 14:34:00 +0900

    feat(promotion): calculate total benefit from order amount related promotions

---
+++ src/main/java/christmas/promotion/byorder/OrderBenefit.java
@@ -0,0 +1,16 @@
+package christmas.promotion.byorder;
+
+import christmas.menu.Price;
+import christmas.promotion.Discount;
+import christmas.promotion.Promotion;
+import christmas.promotion.bydate.Dday;
+
+public class OrderBenefit {
+    public Discount calculateTotalOrderBenefit(Price price) {
+        Promotion promotion = new Promotion(new Dday(), new FreeGift(), new Badge());
+
+        int totalOrderBenefit = promotion.determineGift(price).getPrice();
+
+        return new Discount(totalOrderBenefit);
+    }
+}
