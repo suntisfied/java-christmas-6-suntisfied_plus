ab7b21f196e927edd231ed18af03a361a42c0438
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-10 19:06:53 +0900

    test(MenuConverter): ensure accurate conversion of menu names and numbers string to HashMap for better management

--- src/test/java/christmas/menu/MenuConverterTest.java
+++ src/test/java/christmas/menu/MenuConverterTest.java
@@ -2,7 +2,8 @@ package christmas.menu;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
+import christmas.menu.converter.MenuConverter;
+import java.util.HashMap;
 import org.junit.jupiter.api.Test;
 
 class MenuConverterTest {
@@ -14,12 +15,12 @@ class MenuConverterTest {
     }
 
     @Test
-    public void convertInputsToMenus() {
-        assertThat(menuConverter.createOrderedMenus("티본스테이크,시저샐러드,아이스크림,레드와인"))
-                .isEqualTo(new Ordered(Arrays.asList(
-                        MainDishes.T_BONE_STEAK,
-                        Appetizers.CAESAR_SALAD,
-                        Desserts.ICE_CREAM,
-                        Drinks.RED_WINE)));
+    public void convertInputToMenus() {
+        Ordered orderedMenus = menuConverter.createOrderedMenus("해산물파스타-2,레드와인-1,초코케이크-1");
+        HashMap<Menu, MenuAmount> rawOrderedMenus = orderedMenus.menus();
+
+        assertThat(rawOrderedMenus.get(MainDishes.SEAFOOD_PASTA)).isEqualTo(new MenuAmount(2));
+        assertThat(rawOrderedMenus.get(Drinks.RED_WINE)).isEqualTo(new MenuAmount(1));
+        assertThat(rawOrderedMenus.get(Desserts.CHOCOLATE_CAKE)).isEqualTo(new MenuAmount(1));
     }
 }
