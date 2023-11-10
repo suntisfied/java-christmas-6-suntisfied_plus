ea2f0432a4dff751e02a50b06afba0abcf378672
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-10 13:29:03 +0900

    feat(promotion): calculate total benefit from date-related promotions

---
+++ src/main/java/christmas/promotion/bydate/DateBenefit.java
@@ -0,0 +1,26 @@
+package christmas.promotion.bydate;
+
+import christmas.menu.Price;
+import christmas.promotion.Discount;
+import christmas.promotion.Promotion;
+import christmas.promotion.byorder.Badge;
+import christmas.promotion.byorder.FreeGift;
+import christmas.view.input.Date;
+import java.util.Arrays;
+import java.util.List;
+
+public class DateBenefit {
+    public Discount calculateTotalDateBenefit(Date date) {
+        List<DateDiscount> dateDiscounts = Arrays.asList(new Dday(), new Weekday(), new Weekend(), new Special());
+
+        int totalDateBenefit = 0;
+        for (DateDiscount dateDiscount : dateDiscounts) {
+            Promotion promotion = new Promotion(dateDiscount, new FreeGift(), new Badge());
+            if (promotion.checkDate(date)) {
+                totalDateBenefit += promotion.calculateDiscount(date).amount();
+            }
+        }
+
+        return new Discount(totalDateBenefit);
+    }
+}
