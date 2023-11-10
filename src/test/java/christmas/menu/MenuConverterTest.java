7aeafcc16ea8269eaf6751720259a3a2e8be6a1e
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-10 16:35:01 +0900

    test(MenuConverter): ensure correct convert of multiple input menus string to menu data

--- src/test/java/christmas/menu/MenuConverterTest.java
+++ src/test/java/christmas/menu/MenuConverterTest.java
@@ -2,13 +2,24 @@ package christmas.menu;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class MenuConverterTest {
+    MenuConverter menuConverter = new MenuConverter();
+
     @Test
     public void convertInputToMenu() {
-        MenuConverter menuConverter = new MenuConverter();
-
         assertThat(menuConverter.convertInputToMenu("티본스테이크")).isEqualTo(MainDishes.T_BONE_STEAK);
+    }
+
+    @Test
+    public void convertInputsToMenus() {
+        assertThat(menuConverter.createOrderedMenus("티본스테이크,시저샐러드,아이스크림,레드와인"))
+                .isEqualTo(new Ordered(Arrays.asList(
+                        MainDishes.T_BONE_STEAK,
+                        Appetizers.CAESAR_SALAD,
+                        Desserts.ICE_CREAM,
+                        Drinks.RED_WINE)));
     }
 }
