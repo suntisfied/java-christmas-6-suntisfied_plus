f4c003622dbde0554599356034fde800520bf47c
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-11 19:37:05 +0900

    test(TotalBenefit): ensure accurate calculation of total benefit from promotion

--- src/test/java/christmas/promotion/TotalBenefitTest.java
+++ src/test/java/christmas/promotion/TotalBenefitTest.java
@@ -2,16 +2,18 @@ package christmas.promotion;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import christmas.order.menu.Price;
 import christmas.view.input.Date;
+import christmas.view.input.Order;
 import org.junit.jupiter.api.Test;
 
 class TotalBenefitTest {
+    Order order = new Order("티본스테이크-3,양송이수프-1,아이스크림-2,레드와인-2");
+
     @Test
     public void calculateTotalBenefit() {
         TotalBenefit totalBenefit = new TotalBenefit();
 
-        assertThat(totalBenefit.calculateTotalBenefit(new Date(10), new Price(120000)))
-                .isEqualTo(new Discount(29923));
+        assertThat(totalBenefit.calculateTotalBenefit(new Date(10), order))
+                .isEqualTo(new Discount(31946));
     }
 }
