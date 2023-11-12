276dbe14241a9ae6f6921d23adc00f496c32a21f
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-12 21:47:16 +0900

    feat(Application): implement main program execution

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,23 @@
 package christmas;
 
+import camp.nextstep.edu.missionutils.Console;
+import christmas.view.input.Date;
+import christmas.view.input.DateInput;
+import christmas.view.input.Order;
+import christmas.view.input.OrderInput;
+import christmas.view.output.Preview;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        DateInput dateInput = new DateInput();
+        OrderInput orderInput = new OrderInput();
+        Preview preview = new Preview();
+
+        Date date = dateInput.askDate();
+        Order order = orderInput.askOrder();
+
+        Console.close();
+
+        preview.display(date, order);
     }
 }
