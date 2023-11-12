2bb331547674f0ec5420344832757014c3bdf344
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-12 21:38:41 +0900

    feat(Integrator): integrate information of free gift

--- src/main/java/christmas/Integrator.java
+++ src/main/java/christmas/Integrator.java
@@ -4,12 +4,15 @@ import christmas.order.OrderVolume;
 import christmas.order.TotalOrder;
 import christmas.order.converter.Converter;
 import christmas.order.menu.Menu;
+import christmas.promotion.Promotions;
+import christmas.promotion.byorder.OrderBenefit;
 import christmas.view.Messages;
 import christmas.view.input.Order;
 import java.text.NumberFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 public class Integrator {
     NumberFormat numberFormatter;
@@ -42,5 +45,26 @@ public class Integrator {
 
         return numberFormatter.format(totalOrderCost)
                 + Messages.UNIT_CURRENCY.getMessage();
+    }
+
+    protected String formatFreeGift(Order order) {
+        OrderBenefit orderBenefit = new OrderBenefit();
+
+        String freeGiftText = Promotions.NONE.getText();
+
+        if (!orderBenefit.determineGift(order).freeGifts().isEmpty()) {
+            HashMap<Menu, OrderVolume> rawFreeGifts = orderBenefit.determineGift(order).freeGifts();
+            Set<Menu> freeGiftNames = rawFreeGifts.keySet();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Menu menu : freeGiftNames) {
+                stringBuilder.append(menu.getName())
+                        .append(" ")
+                        .append(rawFreeGifts.get(menu).volume())
+                        .append(Messages.UNIT_COUNT.getMessage());
+            }
+            freeGiftText = stringBuilder.toString();
+        }
+
+        return freeGiftText;
     }
 }
