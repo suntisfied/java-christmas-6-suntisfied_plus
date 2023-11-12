1ee26f5bad99b105e7697321e1603ad3fc5015cd
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-12 11:30:15 +0900

    refactor(order): migrate exception handling from TotalOrder to OrderInput

--- src/main/java/christmas/order/TotalOrder.java
+++ src/main/java/christmas/order/TotalOrder.java
@@ -1,39 +1,18 @@
 package christmas.order;
 
 import christmas.order.converter.Converter;
-import christmas.order.menu.Category;
 import christmas.order.menu.Menu;
 import christmas.order.menu.Price;
 import christmas.view.input.Order;
 import java.util.HashMap;
 import java.util.List;
-import java.util.function.Predicate;
 
 public class TotalOrder {
     private final OrderWithVolume orderWithVolume;
 
     public TotalOrder(Order order) {
         this.orderWithVolume = new Converter().createOrderedMenuTotal(order);
-        validate(order);
     }
-
-    private void validate(Order order) {
-        if (!isWithinOrderLimit.test(order) || isOnlyDrinkOrder.test(order)) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private final Predicate<Order> isWithinOrderLimit = order -> {
-        VolumeCalculator volumeCalculator = new VolumeCalculator();
-        return volumeCalculator.calculateTotalOrderVolume(order).volume() <= 20;
-    };
-
-    private final Predicate<Order> isOnlyDrinkOrder = order -> {
-        VolumeCalculator volumeCalculator = new VolumeCalculator();
-        OrderVolume totalVolume = volumeCalculator.calculateTotalOrderVolume(order);
-        OrderVolume drinkVolume = volumeCalculator.calculateOrderVolumeByCategory(order, Category.DRINK);
-        return totalVolume.equals(drinkVolume);
-    };
 
     public OrderedMenus produceOrderedMenu() {
         return new OrderedMenus(orderWithVolume.orderedMenuTotal().keySet().stream().toList());
