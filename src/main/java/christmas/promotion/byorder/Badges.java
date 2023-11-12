781cc56b0ee9b5a7f128b01a4fb5ee18ffa80fd8
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-12 17:12:06 +0900

    feat(Badges): convert enum members to strings

--- src/main/java/christmas/promotion/byorder/Badges.java
+++ src/main/java/christmas/promotion/byorder/Badges.java
@@ -1,8 +1,18 @@
 package christmas.promotion.byorder;
 
 public enum Badges {
-    NONE,
-    STAR,
-    TREE,
-    SANTA
+    NONE("없음"),
+    STAR("스타"),
+    TREE("트리"),
+    SANTA("산타");
+
+    private final String name;
+
+    Badges(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
