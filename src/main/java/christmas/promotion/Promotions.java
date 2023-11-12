79613175eab2cee13f2bff29244fe247c5180558
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-12 19:00:14 +0900

    refactor(Promotions): allocate text for display to each member

--- src/main/java/christmas/promotion/Promotions.java
+++ src/main/java/christmas/promotion/Promotions.java
@@ -1,10 +1,21 @@
 package christmas.promotion;
 
 public enum Promotions {
-    D_DAY,
-    WEEKDAY,
-    WEEKEND,
-    SPECIAL,
-    FREE_GIFT,
-    BADGE
+    D_DAY("크리스마스 디데이 할인: "),
+    WEEKDAY("평일 할인: "),
+    WEEKEND("주말 할인: "),
+    SPECIAL("특별 할인: "),
+    FREE_GIFT("증정 이벤트: "),
+    BADGE(""),
+    NONE("없음");
+
+    private final String headText;
+
+    Promotions(String text) {
+        this.headText = text;
+    }
+
+    public String getText() {
+        return headText;
+    }
 }
