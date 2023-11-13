afd065cc86adc02ef0e97d335c7a5fe40b47a752
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-13 13:47:27 +0900

    refactor(MenuTableCreator): optimize the process of creating MenuTable

--- src/main/java/christmas/order/converter/MenuTableCreator.java
+++ src/main/java/christmas/order/converter/MenuTableCreator.java
@@ -1,24 +1,25 @@
 package christmas.order.converter;
 
 import christmas.order.menu.Menu;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MenuTableCreator {
     MenuTable createMenuTable() {
+        List<Menu> menus = List.of(Menu.values());
+
+        List<String> menuNames = new ArrayList<>();
+        for (Menu menu : menus) {
+            menuNames.add(menu.getName());
+        }
+
         Map<String, Menu> menuTable = new HashMap<>();
-        menuTable.put(Menu.MUSHROOM_CREAM_SOUP.getName(), Menu.MUSHROOM_CREAM_SOUP);
-        menuTable.put(Menu.TAPAS.getName(), Menu.TAPAS);
-        menuTable.put(Menu.CAESAR_SALAD.getName(), Menu.CAESAR_SALAD);
-        menuTable.put(Menu.T_BONE_STEAK.getName(), Menu.T_BONE_STEAK);
-        menuTable.put(Menu.BARBECUE_RIBS.getName(), Menu.BARBECUE_RIBS);
-        menuTable.put(Menu.SEAFOOD_PASTA.getName(), Menu.SEAFOOD_PASTA);
-        menuTable.put(Menu.CHRISTMAS_PASTA.getName(), Menu.CHRISTMAS_PASTA);
-        menuTable.put(Menu.CHOCOLATE_CAKE.getName(), Menu.CHOCOLATE_CAKE);
-        menuTable.put(Menu.ICE_CREAM.getName(), Menu.ICE_CREAM);
-        menuTable.put(Menu.ZERO_COLA.getName(), Menu.ZERO_COLA);
-        menuTable.put(Menu.RED_WINE.getName(), Menu.RED_WINE);
-        menuTable.put(Menu.CHAMPAGNE.getName(), Menu.CHAMPAGNE);
+        for (int i = 0; i < menuNames.size(); i++) {
+            menuTable.put(menuNames.get(i), menus.get(i));
+        }
+
         return new MenuTable(menuTable);
     }
 }
