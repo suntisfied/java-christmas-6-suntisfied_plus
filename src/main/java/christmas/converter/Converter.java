87be9d56999dd395b176ef2a4093a284fbe13962
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-14 10:50:15 +0900

    refactor(Converter): remove unnecessary field members

--- src/main/java/christmas/converter/Converter.java
+++ src/main/java/christmas/converter/Converter.java
@@ -22,17 +22,9 @@ import java.util.List;
 import java.util.Map;
 
 public class Converter {
-    private final Extractor extractor;
-    private final Separator separator;
-
-    public Converter() {
-        extractor = new Extractor();
-        separator = new Separator();
-    }
-
     public TotalOrder convertToTotalOrder(Order order) {
-        List<String> menuNameAndVolumes = separator.createMenuNameAndVolumes(order);
-        Map<Menu, Volume> orderedMenuTotal = extractor.createMenus(menuNameAndVolumes);
+        List<String> menuNameAndVolumes = new Separator().createMenuNameAndVolumes(order);
+        Map<Menu, Volume> orderedMenuTotal = new Extractor().createMenus(menuNameAndVolumes);
 
         return new TotalOrder(orderedMenuTotal);
     }
