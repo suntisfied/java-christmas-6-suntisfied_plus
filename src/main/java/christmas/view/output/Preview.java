8e91667b3036becfe5bfe402a3c65dd284652c8c
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-14 13:44:34 +0900

    refactor(Preview): remove unnecessary field members

--- src/main/java/christmas/view/output/Preview.java
+++ src/main/java/christmas/view/output/Preview.java
@@ -10,21 +10,13 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Preview {
-    private final OrderPreview orderPreview;
-    private final PromotionPreview promotionPreview;
-
-    public Preview() {
-        this.orderPreview = new OrderPreview();
-        this.promotionPreview = new PromotionPreview();
-    }
-
     public void display(Date date, Order order) {
         System.out.println(buildPreview(date, order));
     }
 
     public String buildPreview(Date date, Order order) {
-        Map<String, String> previews = new LinkedHashMap<>(orderPreview.build(order));
-        previews.putAll(promotionPreview.build(date, order));
+        Map<String, String> previews = new LinkedHashMap<>(new OrderPreview().build(order));
+        previews.putAll(new PromotionPreview().build(date, order));
 
         String previewText = previews.entrySet().stream()
                 .flatMap(entry -> Stream.of(entry.getKey() + "\r\n", entry.getValue() + "\r\n\r\n"))
