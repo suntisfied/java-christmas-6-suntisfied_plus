774d5857f8350d3e54a2d575fda7bdf2e32c1a4f
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-15 17:22:55 +0900

    refactor(Badge): remove unnecessary function

--- src/main/java/christmas/promotion/Badge.java
+++ src/main/java/christmas/promotion/Badge.java
@@ -8,32 +8,22 @@ import static christmas.promotion.Badges.TREE;
 import christmas.converter.Converter;
 import christmas.view.input.Date;
 import christmas.view.input.Order;
-import java.util.function.Predicate;
 
 public class Badge extends Benefit {
     public Badges determineBadge(Date date, Order order) {
         Discount discount = calculateTotalDiscount(date, order);
 
-        if (check(date, order)) {
-            if (discount.amount() >= SANTA.getRequiredOrder()) {
-                return SANTA;
-            }
-            if (discount.amount() >= TREE.getRequiredOrder()) {
-                return TREE;
-            }
-            if (discount.amount() >= STAR.getRequiredOrder()) {
-                return STAR;
-            }
+        if (discount.amount() >= SANTA.getRequiredOrder()) {
+            return SANTA;
+        }
+        if (discount.amount() >= TREE.getRequiredOrder()) {
+            return TREE;
+        }
+        if (discount.amount() >= STAR.getRequiredOrder()) {
+            return STAR;
         }
         return NONE;
     }
-
-    protected boolean check(Date date, Order order) {
-        Discount discount = calculateTotalDiscount(date, order);
-        return isEnough.test(discount);
-    }
-
-    private final Predicate<Discount> isEnough = discount -> discount.amount() >= STAR.getRequiredOrder();
 
     private Discount calculateTotalDiscount(Date date, Order order) {
         TotalBenefit totalBenefit = new Converter().convertToTotalBenefit(date, order);
